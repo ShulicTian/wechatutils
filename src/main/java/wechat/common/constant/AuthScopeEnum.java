0dbb637abdcecfc71437c333863fd5918a38f317
git <609417423@qq.com>
2024-03-29 14:18:34 +0800

    bug修复

--- src/main/java/wechat/common/constant/AuthScopeEnum.java
+++ src/main/java/wechat/common/constant/AuthScopeEnum.java
@@ -7,6 +7,7 @@ package wechat.common.constant;
 public enum AuthScopeEnum {
     SNS_API_BASE("snsapi_base"),
     SNS_API_USERINFO("snsapi_userinfo"),
+    SNS_API_PRIVATE_INFO("snsapi_privateinfo"),
     SNS_API_LOGIN("snsapi_login");
 
     private final String scope;
